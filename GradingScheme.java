4a62b5b824d8ef562156f90808f6ae08b625d5f2
Charlize Amorato <charr.lizzee@gmail.com>
2022-06-28 13:44:18 +0800

    TIP Grading System : Initial Commit

---
+++ GradingScheme.java
@@ -0,0 +1,17 @@
+public class GradingScheme {
+    private int percentGrade;
+    private double equivalentGrade;
+    private String descriptiveRating;
+
+    // constructor
+    public GradingScheme(int percent, double equivalent, String rating) {
+        this.percentGrade = percent;
+        this.equivalentGrade = equivalent;
+        this.descriptiveRating = rating;
+    }
+
+    // setters and getters
+
+    // arrays
+
+}
